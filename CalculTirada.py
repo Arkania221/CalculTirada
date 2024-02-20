64760a7bc5c2c02844ce7f99d194b134ef3a3a9a
Ángel Borrás <angeloki1963@gmail.com>
2024-02-20 22:17:50 +0100

    Destripando la practica

---
+++ CalculTirada.py
@@ -0,0 +1,19 @@
+"""
+
+1. ¿Que se necesita? Se precisa saber la probabilidad de que toque cierto resultado
+   mas los posibles resultados menores.
+
+2. Los posibles resultados vienen dados de la suma de dos dados de seis caras 
+   (2 - 12)
+
+3. El resultado a operar sera ofrecido por el usuario, se debe invalidad cualquier
+   dato dado por el usuario que ses salga fuera del rango.
+
+
+4. Para calcular su porcentaje de probabilidad se debe realizar la siguiente 
+   operacion: [Suma de la cantidad de posibles resultados que den X o inferior]
+   * 100 / 36 
+
+5. Se debe mostrar por pantalla el porcentaje de probabilidad del numero.
+
+"""
